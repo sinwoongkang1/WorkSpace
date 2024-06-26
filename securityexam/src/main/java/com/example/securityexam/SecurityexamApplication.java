5e052599ed9db9aa9bf02ec661a2587d564787c6
sinwoongkang1 <kkace22@gmail.com>
2024-06-26 18:12:09 +0900

    1

---
+++ securityexam/src/main/java/com/example/securityexam/SecurityexamApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityexamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityexamApplication.class, args);
+    }
+
+}
